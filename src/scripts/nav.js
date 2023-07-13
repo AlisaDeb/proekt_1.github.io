aa70217997a37507773c8af02757a508a029eccf
AlisaDeb <alisashd8@gmail.com>
2023-07-13 17:06:20 +0200

    Update nav.js

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,12 +1,15 @@
 const buttonBurger = document.querySelector('.js-button-burger')
 const navHeader = document.querySelector('.js-nav')
 
+const body = document.body
+
 buttonBurger.addEventListener('click', (event) => {
     
-    if(navHeader.classList.contains('nav_active')) {
-        navHeader.classList.remove('nav_active')
-    } else {
+    if(!navHeader.classList.contains('nav_active')) {
+        body.style.overflow = 'hidden'
         navHeader.classList.add('nav_active')
+    } else {
+        navHeader.classList.remove('nav_active')
     }
-    
+
 })
